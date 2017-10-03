eeaf79d38a77115efc9589047a3f1fb56836df9c
NullDev (Shadow) <NL-Dev@yandex.com>
2017-10-03 21:44:44 +0200

    Automated execution

--- app.js
+++ app.js
@@ -1,48 +1,61 @@
 "use strict";
-
 var github = require('octokat');
+var cron   = require('cron').CronJob;
+var fs     = require('fs');
+
+////////////////////////////////
+//----------------------------//
+// Copyright (c) 2017 NullDev //
+//----------------------------//
+////////////////////////////////
 
 require.extensions['.json'] = function (module, filename) { module.exports = fs.readFileSync(filename, 'utf8'); };
 var jsondata = require('./config.json');
 var raw      = JSON.parse(jsondata);
 
-var user = raw.auth.username;
-var pass = raw.auth.password;
+var time = raw.cron.time;
+var TZ   = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-var repo = raw.file.repository;
-var file = raw.file.streakfile;
+var crontime = "0 " + time + " * * *";
 
-var git = new github({
-	username: user,
-	password: pass
-});
- 
-var r = git.repos(user, repo);
+new cron(crontime, function() {
+	var user = raw.auth.username,
+		pass = raw.auth.password;
 
-r.contents(file).fetch().then((i) => {
-	var s = i.content;
-	var b = Buffer.from(s, 'base64');
+	var repo = raw.file.repository,
+		file = raw.file.streakfile;
 
-	var fin = b.toString();
-	var sha = i.sha;
-	doCommit(fin, sha);
-});
-
-function doCommit(str, sha){
-	str = parseInt(str);
-	var day = ++str;
-
-	var buf = new Buffer(day.toString());
-	var stk = buf.toString('base64');
-
-	var config = {
-		message: 'Streak Day ' + day,
-		content: stk,
-		sha: sha
-	}
-
-	r.contents(file).add(config).then((info) => {
-		console.log("Day " + day + ": New SHA is " + info.commit.sha);
-		process.exit(0);
+	var git = new github({
+		username: user,
+		password: pass
 	});
-}
+
+	var r = git.repos(user, repo);
+
+	r.contents(file).fetch().then((i) => {
+		var s = i.content;
+		var b = Buffer.from(s, 'base64');
+		var fin = b.toString();
+		var sha = i.sha;
+		doCommit(fin, sha);
+	});
+
+	function doCommit(str, sha){
+		str = parseInt(str);
+		var day = ++str;
+
+		var buf = new Buffer(day.toString());
+		var stk = buf.toString('base64');
+
+		var config = {
+			message: 'Streak Day ' + day,
+			content: stk,
+			sha: sha
+		}
+
+		r.contents(file).add(config).then((info) => {
+			console.log("Day " + day + ": New SHA is " + info.commit.sha);
+			process.exit(0);
+		});
+	}
+}, null, true, TZ);
