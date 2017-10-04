f1339652ae3f098c9a42fa27be702dc04de4596f
NullDev (Shadow) <NL-Dev@yandex.com>
2017-10-04 12:13:04 +0200

    Added log mute argument

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var fs     = require('fs');
 //----------------------------//
 ////////////////////////////////
 
+var mutelog = false;
+
 require.extensions['.json'] = function (module, filename) { module.exports = fs.readFileSync(filename, 'utf8'); };
 var jsondata = require('./config.json');
 var raw      = JSON.parse(jsondata);
@@ -21,9 +23,15 @@ var crontime = "0 " + time + " * * *";
 var hrs = new Date().getHours(),
 	min = new Date().getMinutes();
 
-function log(text, init){ 
-	if (!init) console.log(getTS() + "\xa0" + text); 
-	function nl(){ console.log("\xa0"); }
+var args = process.argv;
+args = args.map(v => v.toLowerCase());
+
+if (args.indexOf("-m") > -1 || args.indexOf("--mute")   > -1) mutelog = true;
+if (args.indexOf("-c") > -1 || args.indexOf("--commit") > -1) performCron(); 
+
+function log(text, init){
+	if (!mutelog) if (!init) console.log(getTS() + "\xa0" + text); 
+	function nl(){ if (!mutelog) console.log("\xa0"); }
 	log.nl = nl;
 }
 log(null, true);
@@ -35,10 +43,6 @@ log("Current time: " + hrs + ":" + min + " (" + toFormat(hrs, min) + ")")
 log("Executing at: " + time + ":00 (" + toFormat(time, 0) + ")");
 log("Timezone: " + TZ);
 log.nl();
-
-var args = process.argv;
-args = args.map(v => v.toLowerCase());
-if (args.indexOf("-c") > -1 || args.indexOf("--commit") > -1) performCron();
 
 function performCron(){
 	var user = raw.auth.username,
